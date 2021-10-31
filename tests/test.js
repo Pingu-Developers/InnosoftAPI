3e21aadbf456ca8c4de4e1660f1743f6db2f5500
alesancor1 <alesancor1@alum.us.es>
2021-10-31 12:37:41 +0100

    added tests

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+describe("Test", function() {
+
+    it("Should pass", function() {
+        assert.equal(1, 1);
+    });
+
+    it("Should also pass", function() {
+        assert.ok(true);
+    });
+
+});
