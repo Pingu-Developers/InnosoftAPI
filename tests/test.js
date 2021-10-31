7d7ed39794c190c11679b7d3fa0da444a4ede8f7
alesancor1 <alesancor1@alum.us.es>
2021-10-31 13:04:17 +0100

    test: Removed sinon

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,10 @@
 const assert = require('assert');
 const server = require('../server');
-const sinon = require('sinon');
 
 describe("Innosoft API Tests:", function() {
 
     before( (done) => {
         server.deploy('test').then(() => {
-            sinon.stub(console);
             done();
         }).catch( (err) => {
             console.log(err);
