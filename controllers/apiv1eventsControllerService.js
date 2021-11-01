2cfbad8cb4d9bb7bc3129e3f8956122051206c50
alesancor1 <alesancor1@alum.us.es>
2021-11-01 19:18:10 +0100

    lint fix

--- controllers/apiv1eventsControllerService.js
+++ controllers/apiv1eventsControllerService.js
@@ -1,28 +1,28 @@
 'use strict';
 
-var db = require('../utils/dbConnection')
+const db = require('../utils/dbConnection');
 
 module.exports.getEvents = function getEvents (req, res, next) {
-  var sql = `SELECT posts.ID,post_title,start,end,time_start,time_end 
+  const sql = `SELECT posts.ID,post_title,start,end,time_start,time_end 
     FROM wp_posts posts RIGHT JOIN wp_mec_events ev on posts.ID = ev.post_id 
     WHERE post_type = 'mec-events' and post_status not like 'auto-draft';`;
-  
+
   db.query(sql).then((result) => {
     result = result.map((item) => {
-      item.start = item.start == '0000-00-00' ? new Date() : new Date(`${item.start}`);
-      item.start.setSeconds(item.time_start);  
-      item.end = item.end == '0000-00-00' ? item.start : new Date(`${item.end}`);
+      item.start = item.start === '0000-00-00' ? new Date() : new Date(`${item.start}`);
+      item.start.setSeconds(item.time_start);
+      item.end = item.end === '0000-00-00' ? item.start : new Date(`${item.end}`);
       item.end.setSeconds(item.time_end);
-      
+
       return {
         eventId: item.ID,
         eventName: item.post_title,
         eventStartDateTime: item.start.toISOString(),
         eventEndDateTime: item.end.toISOString()
-      }
+      };
     });
     res.status(200).send(result);
   }).catch((err) => {
-    res.status(500).send({message: err.message});
-  }); 
+    res.status(500).send({ message: err.message });
+  });
 };
--- utils/dbMockup.js
+++ utils/dbMockup.js
@@ -2,15 +2,15 @@ const sinon = require('sinon');
 const mock = sinon.mock(require('./dbConnection'));
 
 exports.query = (query, output, params) => {
-    if (params) {
-        mock.expects('query').withExactArgs(query, params).resolves(output);
-    } else {
-        mock.expects('query').withExactArgs(query).resolves(output);
-    }
-}
+  if (params) {
+    mock.expects('query').withExactArgs(query, params).resolves(output);
+  } else {
+    mock.expects('query').withExactArgs(query).resolves(output);
+  }
+};
 exports.restore = () => {
-    mock.restore();
-}
+  mock.restore();
+};
 exports.verify = () => {
-    mock.verify();
-}
+  mock.verify();
+};
