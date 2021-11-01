b838cfb6dcb911cb7e8f2d9a82ffc740237a6a94
alesancor1 <alesancor1@alum.us.es>
2021-11-01 19:14:40 +0100

    test: Database mockup for tests

---
+++ utils/dbMockup.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const mock = sinon.mock(require('./dbConnection'));
+
+exports.query = (query, output, params) => {
+    if (params) {
+        mock.expects('query').withExactArgs(query, params).resolves(output);
+    } else {
+        mock.expects('query').withExactArgs(query).resolves(output);
+    }
+}
+exports.restore = () => {
+    mock.restore();
+}
+exports.verify = () => {
+    mock.verify();
+}
