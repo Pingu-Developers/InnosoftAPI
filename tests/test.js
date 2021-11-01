86a886d7d609edb659d1082c6d3816068fef049f
alesancor1 <alesancor1@alum.us.es>
2021-11-01 19:14:59 +0100

    test: Implemented tests for events endpoint

--- tests/test.js
+++ tests/test.js
@@ -1,28 +1,47 @@
 const assert = require('assert');
+const axios = require('axios');
 const server = require('../server');
+const mock = require('../utils/dbMockup');
+const deref = require('json-schema-deref-sync');
+
+/* Declaring const vars */
+const tests = deref(require('./test.json')).tests;
+const host = "http://localhost:5000"
 
 describe("Innosoft API Tests:", function() {
 
+    /* EXECUTED BEFORE ALL TESTS */
     before( (done) => {
         server.deploy('test').then(() => {
             done();
         }).catch( (err) => {
             console.log(err);
-            done(err);;
+            done(err);
         })
     });
 
-    describe("Testing endpoint /api/v1/test", () => {
-        it("Should always pass", function() {
-            assert.equal(1, 1);
-        });
-    
-        it("Should also always pass", function() {
-            assert.ok(true);
-        });
+    /* ENDPOINT TESTS */
+    describe("Testing endpoint /api/v1/events : ", () => {
+        apiEventPositiveTest();
     });
 
+    function apiEventPositiveTest() {  
+        for (let test of tests["api/v1/events"]) {
+            it (test.description , async () => {
+                mock.query(test.query, test.result);
+                await axios.get(host + '/api/v1/events').then((response) => {
+                    assert.equal(JSON.stringify(response.data), JSON.stringify(test.response));
+                }).catch((err) => {
+                    console.log(err.response.data);
+                    assert.fail(`Error on request`);
+                });
+            });
+        }
+    }
+
+    /* EXECUTED AFTER ALL TESTS */
     after((done) => {
+        mock.restore();
         server.undeploy(done);
     });
 });
