ba3a61553399feee0fbe94108d4e4185e63ff12f
alesancor1 <alesancor1@alum.us.es>
2021-11-01 21:11:24 +0100

    test: Added negative case tests

--- tests/test.js
+++ tests/test.js
@@ -21,15 +21,22 @@ describe("Innosoft API Tests:", function() {
     });
 
     /* ENDPOINT TESTS */
-    describe("Testing endpoint /api/v1/events : ", () => {
+    describe("[Positive Case] Testing endpoint /api/v1/events : ", () => {
+        after( () => mock.restore());
         apiEventPositiveTest();
     });
 
-    function apiEventPositiveTest() {  
-        for (let test of tests["api/v1/events"]) {
-            it (test.description , async () => {
-                mock.query(test.query, test.result);
-                await axios.get(host + '/api/v1/events').then((response) => {
+    describe("[Negative Case] Testing endpoint /api/v1/events : ", () => {
+        after( () => mock.restore());
+        apiEventNegativeTest();
+    });
+
+    function apiEventPositiveTest() {
+        var apitest = tests["api/v1/events"];
+        for (let test of apitest.cases) {
+            it (test.description , () => {
+                mock.query(apitest.query, test.result);
+                return axios.get(host + '/api/v1/events').then((response) => {
                     assert.equal(JSON.stringify(response.data), JSON.stringify(test.response));
                 }).catch((err) => {
                     console.log(err.response.data);
@@ -38,10 +45,27 @@ describe("Innosoft API Tests:", function() {
             });
         }
     }
+    function apiEventNegativeTest() {
+        var apitest = tests["api/v1/events"];
+        it ("Should respond with code 500 when database is down", () => {
+            return axios.get(host + '/api/v1/events').then(() => {
+                assert.fail(`Error on request`);
+            }).catch((err) => {
+                assert.equal(err.response.status, 500);
+            });
+        });
+        it ("Should respond with code 500 method fails", () => {
+            mock.query(apitest.query, null);
+            return axios.get(host + '/api/v1/events').then(() => {
+                assert.fail(`Error on request`);
+            }).catch((err) => {
+                assert.equal(err.response.status, 500);
+            });
+        });
+    }
 
     /* EXECUTED AFTER ALL TESTS */
     after((done) => {
-        mock.restore();
         server.undeploy(done);
     });
 });
