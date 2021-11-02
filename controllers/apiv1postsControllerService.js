363a5f2f73fd7b2b9c19763d4e0b71ff675a6515
alesancor1 <alesancor1@alum.us.es>
2021-11-02 16:19:11 +0100

    feat: Implemented getPosts endpoint

--- controllers/apiv1postsControllerService.js
+++ controllers/apiv1postsControllerService.js
@@ -1,7 +1,24 @@
 'use strict';
 
+const db = require('../utils/dbConnection');
+
 module.exports.getPosts = function getPosts (req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for getPosts'
+  const sql = `SELECT p.ID,post_title,post_content,post_date,display_name FROM wp_posts p 
+  RIGHT JOIN wp_users u ON post_author = u.ID WHERE post_type='post' AND post_status='publish' 
+  ORDER BY p.post_date DESC;`; 
+  
+  db.query(sql).then((result) => {
+    const posts = result.map((item) => {
+      return {
+        postId: item.ID,
+        postTitle: item.post_title,
+        postContent: item.post_content,
+        postDateTime: new Date(item.post_date).toISOString(),
+        postAuthor: item.display_name
+      };
+    });
+    res.status(200).send(posts);
+  }).catch((err) => {
+    res.status(500).send({ message: err.message });
   });
 };
