322f869376d919c6257058c68ed963481fe82fc4
alesancor1 <alesancor1@alum.us.es>
2021-11-02 16:20:27 +0100

    Lint fix

--- controllers/apiv1postsControllerService.js
+++ controllers/apiv1postsControllerService.js
@@ -5,8 +5,8 @@ const db = require('../utils/dbConnection');
 module.exports.getPosts = function getPosts (req, res, next) {
   const sql = `SELECT p.ID,post_title,post_content,post_date,display_name FROM wp_posts p 
   RIGHT JOIN wp_users u ON post_author = u.ID WHERE post_type='post' AND post_status='publish' 
-  ORDER BY p.post_date DESC;`; 
-  
+  ORDER BY p.post_date DESC;`;
+
   db.query(sql).then((result) => {
     const posts = result.map((item) => {
       return {
