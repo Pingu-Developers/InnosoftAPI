3e461bd593623f1c793ea1b6188450fe96b0d3e2
alesancor1 <alesancor1@alum.us.es>
2021-11-02 01:22:33 +0100

    fix: Results are not overwritten anymore

--- controllers/apiv1eventsControllerService.js
+++ controllers/apiv1eventsControllerService.js
@@ -8,20 +8,20 @@ module.exports.getEvents = function getEvents (req, res, next) {
     WHERE post_type = 'mec-events' and post_status not like 'auto-draft';`;
 
   db.query(sql).then((result) => {
-    result = result.map((item) => {
-      item.start = item.start === '0000-00-00' ? new Date() : new Date(`${item.start}`);
-      item.end = item.end === '0000-00-00' ? new Date(`${item.start}`) : new Date(`${item.end}`);
-      item.start.setSeconds(item.time_start);
-      item.end.setSeconds(item.time_end);
+    var events = result.map((item) => {
+      var start = item.start === '0000-00-00' ? new Date() : new Date(`${item.start}`);
+      var end = item.end === '0000-00-00' ? new Date(`${item.start}`) : new Date(`${item.end}`);
+      start.setSeconds(item.time_start);
+      end.setSeconds(item.time_end);
 
       return {
         eventId: item.ID,
         eventName: item.post_title,
-        eventStartDateTime: item.start.toISOString(),
-        eventEndDateTime: item.end.toISOString()
+        eventStartDateTime: start.toISOString(),
+        eventEndDateTime: end.toISOString()
       };
     });
-    res.status(200).send(result);
+    res.status(200).send(events);
   }).catch((err) => {
     res.status(500).send({ message: err.message });
   });
